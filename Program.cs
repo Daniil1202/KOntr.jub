cb82b79fa78330103a5113e4b3c56ed38f831ee8
Daniil <korostindi@mail.ru>
2023-05-19 16:32:32 +0400

    добавление функции First array

--- Program.cs
+++ Program.cs
@@ -9,4 +9,18 @@
 //  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
+string[] array1 = new string[5] {"123", "hello", "world", "res", "YYYhhhh"};
+string[] array2 = new string[array1.Length];
+void FirstArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
 
